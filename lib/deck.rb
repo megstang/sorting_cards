0cb6d1c09d17143b8edf6ccee6346d5e3e973d12
Meghan Stang <mstang15@gmail.com>
2018-08-21 16:14:39 -0600

    Adds very incomplete merge_sort

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,10 @@
-
+require 'pry'
 class Deck
   attr_reader :cards,
               :count,
-              :sort
+              :sort,
+              :merge_sort,
+              :bubble_sort
 
   def initialize (cards)
     @cards = cards
@@ -12,21 +14,53 @@ class Deck
     @cards.count
   end
 
-  def sort
-    return @cards if count <= 1
+  def bubble_sort(array)
+    return array if array.size <= 1
     swap = true
-      while swap 
+      while swap
         swap = false
-        (count - 1).times do |i|
-          if @cards[i].numeric_value == @cards[i + 1].numeric_value && @cards[i].numeric_suit > @cards[i+1].numeric_suit
-            @cards[i],@cards[i+1] = @cards[i+1], @cards[i]
-          elsif @cards[i].numeric_value > @cards[i + 1].numeric_value
-            @cards[i],@cards[i+1] = @cards[i+1], @cards[i]
+        (array.size - 1).times do |i|
+          if array[i].numeric_value == array[i + 1].numeric_value && array[i].numeric_suit > array[i+1].numeric_suit
+            array[i],array[i+1] = array[i+1], array[i]
+          elsif array[i].numeric_value > array[i + 1].numeric_value
+            array[i],array[i+1] = array[i+1], array[i]
             swap = true
           end
         end
       end
-      @cards
+      array
   end
 
+  def merge(left_array,right_array)
+
+    if right_array.empty?
+      return left_array
+    end
+
+    if left_array.empty?
+      return right_array
+    end
+    sorted = []
+    until left_array.empty? || right_array.empty?
+      if left_array.first.numeric_value <= right_array.first.numeric_value
+        sorted << left_array.shift
+      else
+        sorted << right_array.shift
+      end
+    end
+    bubble_sort(sorted.concat(left_array).concat(right_array))
+  end
+
+  def merge_sort
+    return @cards if count <= 1
+    half_count = (count/2).round
+
+    left_array = @cards.take(half_count)
+    right_array = @cards.drop(half_count)
+
+    sorted_left_array = bubble_sort(left_array)
+    sorted_right_array = bubble_sort(right_array)
+    merge(sorted_left_array,sorted_right_array)
+
+  end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -31,7 +31,8 @@ class DeckTest < Minitest::Test
     assert_equal 3, deck.count
   end
 
-  def test_it_can_sort
+  def test_it_can_bubble_sort
+    skip
     card_1 = Card.new("4","Hearts")
     card_2 = Card.new("Jack", "Clubs")
     card_3 = Card.new("5", "Diamonds")
@@ -41,6 +42,19 @@ class DeckTest < Minitest::Test
     card_7 = Card.new("4","Clubs")
     card_8 = Card.new("5","Spades")
     deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8])
-    assert_equal [card_7, card_1, card_3, card_8, card_2, card_6, card_5, card_4],deck.sort
+    assert_equal [card_7, card_1, card_3, card_8, card_2, card_6, card_5, card_4],deck.bubble_sort
+  end
+
+  def test_it_can_merge_sort
+    card_1 = Card.new("4","Hearts")
+    card_2 = Card.new("Jack", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    card_4 = Card.new("Ace", "Spades")
+    card_5 = Card.new("Ace", "Diamonds")
+    card_6 = Card.new("Queen","Diamonds")
+    card_7 = Card.new("4","Clubs")
+    card_8 = Card.new("5","Spades")
+    deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8])
+    assert_equal [card_7, card_1, card_3, card_8, card_2, card_6, card_5, card_4],deck.merge_sort
   end
 end
