30eac5a1d3a4ec7d8a80b5c72764f5cb61423822
Meghan Stang <mstang15@gmail.com>
2018-08-21 16:52:19 -0600

    adds more tests to card_test

--- test/card_test.rb
+++ test/card_test.rb
@@ -18,4 +18,28 @@ class CardTest < Minitest::Test
     assert_equal "Spades", card.suit
   end
 
+  def test_all_values_have_numeric_value
+    card = Card.new("Ace","Spades")
+    assert_equal 14, card.numeric_value
+    card = Card.new("Jack","Clubs")
+    assert_equal 11, card.numeric_value
+    card = Card.new("3","Diamonds")
+    assert_equal 3, card.numeric_value
+    card = Card.new("Queen", "Spades")
+    assert_equal 12, card.numeric_value
+    card = Card.new("King", "Hearts")
+    assert_equal 13, card.numeric_value
+  end
+
+  def test_all_suits_have_numeric_suits
+    card = Card.new("Ace","Spades")
+    assert_equal 4, card. numeric_suit
+    card = Card.new("Jack","Clubs")
+    assert_equal 1, card. numeric_suit
+    card = Card.new("3","Diamonds")
+    assert_equal 2, card. numeric_suit
+    card = Card.new("King", "Hearts")
+    assert_equal 3, card. numeric_suit
+  end
+
 end
