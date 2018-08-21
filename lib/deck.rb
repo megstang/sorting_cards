60d8caf71fb133d63d8843ad2c7b18c589e2862f
Meghan Stang <mstang15@gmail.com>
2018-08-20 20:46:30 -0600

    Adds Iteration 3

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,29 @@
 
 class Deck
-  attr_reader :cards, :count
-    def initialize (cards)
-      @cards = cards
-    end
+  attr_reader :cards, :count, :sort
+  def initialize (cards)
+    @cards = cards
+  end
 
-    def count
-      @cards.count
-    end
+  def count
+    @cards.count
+  end
+
+  def sort
+    return @cards if count <= 1
+    swap = true
+      while swap #put condition with while while swap ==true, when swap becomes false it will break out of hte loop
+        swap = false
+        (count - 1).times do |i|
+          if @cards[i].numeric_value == @cards[i + 1].numeric_value && @cards[i].numeric_suit > @cards[i+1].numeric_suit
+            @cards[i],@cards[i+1] = @cards[i+1], @cards[i]
+          elsif @cards[i].numeric_value > @cards[i + 1].numeric_value
+            @cards[i],@cards[i+1] = @cards[i+1], @cards[i]
+            swap = true
+          end
+        end
+      end
+      @cards
+  end
 
 end
